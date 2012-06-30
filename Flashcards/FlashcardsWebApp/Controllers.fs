09ab9b25a246745e8afc1e3356158fb721a7b3a0
tkellogg <timothy.kellogg@gmail.com>
2012-06-30 00:56:16 -0600

    Got create & read operations working with a couple entities

---
+++ Flashcards/FlashcardsWebApp/Controllers.fs
@@ -0,0 +1,34 @@
+﻿namespace Flashcards.Controllers
+
+open System.Web
+open System.Web.Mvc
+open MongoDB.Driver
+open Flashcards.Models
+
+[<HandleError>]
+type HomeController() =
+    inherit Controller()
+
+    member this.Index () =
+        this.View() :> ActionResult
+
+[<HandleError>]
+type DeckController() =
+    inherit Controller()
+
+    let db = MongoDatabase.Create "mongodb://localhost/test"
+    let collection = db.GetCollection<Deck> ("decks")
+
+    do
+        Setups.SetupMongo()
+
+    member this.Index () =
+        collection.FindAll () |> List.ofSeq |> this.View
+
+    member this.Add() =
+        this.View()
+
+    [<HttpPostAttribute>]
+    member this.Add (deck : Deck) =
+        collection.Save deck |> ignore
+        this.RedirectToAction "Index"
---
+++ Flashcards/FlashcardsWebApp/Models.fs
@@ -0,0 +1,31 @@
+﻿namespace Flashcards.Models
+
+open MongoDB.Bson
+open MongoDB.Bson.Serialization
+open MongoDB.Bson.Serialization.Attributes
+
+type Side = { Text : string }
+
+type public Card = { Sides : int list }
+
+type Deck() =  
+    let mutable cards = []
+    let mutable id = BsonObjectId.GenerateNewId()
+    let mutable title = "N/A"
+
+    member this.Id
+        with get() = id
+        and set value = id <- value
+
+    member this.Cards
+        with get() = cards
+        and set value = cards <- value
+
+    member this.Title
+        with get() = title
+        and set value = title <- value
+
+module Setups =
+    let SetupMongo () =
+        if BsonClassMap.IsClassMapRegistered(typedefof<Deck>) |> not then
+            BsonClassMap.RegisterClassMap<Deck>() |> ignore
